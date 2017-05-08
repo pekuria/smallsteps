00fc043067e212fd314c4c46493be52f44f5b521
pekuria <pekuria@hotmail.com>
2017-05-08 19:38:48 +0100

    initial update

--- php/app/config/params.php
+++ php/app/config/params.php
@@ -1,8 +1,16 @@
 <?php
 # app/config/params.php
 
+$container->setParameter('database_host', "127.8.226.130");
+$container->setParameter('database_port', "3306");
+$container->setParameter('database_name', "smallsteps");
+$container->setParameter('database_user', "adminV2KTcGU");
+$container->setParameter('database_password', "pMUc2ui2GYhB");
+
+/*
 $container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
 $container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
 $container->setParameter('database_name', getEnv("OPENSHIFT_APP_NAME"));
 $container->setParameter('database_user', getEnv("OPENSHIFT_MYSQL_DB_USERNAME"));
 $container->setParameter('database_password', getEnv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+*/
